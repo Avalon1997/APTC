5d84985468e7d73e086101c5e83fd556b72c9789
avalon1997 <461879033@qq.com>
2026-01-20 10:02:25 +0800

    CubeIDE Project Config Changed

---
+++ YeMing/Control/aptc_core.c
@@ -0,0 +1,29 @@
+/**
+******************************************************************************
+* @file            : core.c
+* @brief           : 
+* @author          : CharlesYu
+* @date            : 2026年1月20日
+******************************************************************************
+* @attention
+*
+* @copyright (c) 2025 by Charles/https://www.charlesyu1997.com
+*
+* All rights reserved.
+*
+******************************************************************************
+*/
+/* Include ---------------------------------------------------------------- */
+// 1. Standard library headers
+
+// 2. STM32 HAL library headers
+
+// 3. Project-wide configuration headers
+
+// 4. The header for this module
+
+// 5. Headers from other modules this one depends on
+
+/* Variable define -------------------------------------------------------- */
+
+/* Functions define ------------------------------------------------------- */
--- YeMing/Core/Src/stm32f1xx_it.c
+++ YeMing/Core/Src/stm32f1xx_it.c
@@ -55,6 +55,8 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern DMA_HandleTypeDef hdma_adc1;
+extern TIM_HandleTypeDef htim4;
 extern DMA_HandleTypeDef hdma_usart1_rx;
 extern DMA_HandleTypeDef hdma_usart1_tx;
 extern UART_HandleTypeDef huart1;
@@ -201,6 +203,20 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
+  * @brief This function handles DMA1 channel1 global interrupt.
+  */
+void DMA1_Channel1_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_adc1);
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 1 */
+}
+
+/**
   * @brief This function handles DMA1 channel4 global interrupt.
   */
 void DMA1_Channel4_IRQHandler(void)
@@ -226,6 +242,20 @@ void DMA1_Channel5_IRQHandler(void)
   /* USER CODE BEGIN DMA1_Channel5_IRQn 1 */
 
   /* USER CODE END DMA1_Channel5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM4 global interrupt.
+  */
+void TIM4_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM4_IRQn 0 */
+
+  /* USER CODE END TIM4_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim4);
+  /* USER CODE BEGIN TIM4_IRQn 1 */
+
+  /* USER CODE END TIM4_IRQn 1 */
 }
 
 /**
